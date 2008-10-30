dca55c85e916efa99403317d1ce6c4c0b8dff1b2
com.chinaos.snaill <com.chinaos.snaill@db7a99aa-9dca-11dd-b92e-bd4787f9249a>
2008-10-30 03:04:28 +0000

    采用SDL_draw库实现基本绘制功能

--- trunk/SDL_MessageBox.cpp
+++ trunk/SDL_MessageBox.cpp
@@ -22,6 +22,7 @@
 #include "SDL_BoxLayout.h"
 #include "SDL_Button.h"
 #include "SDL_Text.h"
+#include <SDL_draw.h>
 
 SDL_MessageBox::SDL_MessageBox( const char * szTitle, const char * szMsg )
 {
@@ -47,18 +48,18 @@ void SDL_MessageBox::GetClientRect( SDL_Rect *rc )
 
 void SDL_MessageBox::DrawWidget( SDL_Surface * screen )
 {
-    DrawRect( screen, m_rc, SDL_MapRGB(screen->format, 192, 192, 192) );
+    Draw_Rect( screen, m_rc.x, m_rc.y, m_rc.w, m_rc.h, SDL_MapRGB(screen->format, 192, 192, 192) );
 
     SDL_Rect    rect;
     rect.x = m_rc.x + 1;
     rect.y = m_rc.y + 1;
     rect.w = m_rc.w - 2;
     rect.h = m_rc.h - 2;
-    DrawRect( screen, rect, SDL_MapRGB(screen->format, 0, 0, 0) );
+    Draw_Rect( screen, rect.x, rect.y, rect.w, rect.h, SDL_MapRGB(screen->format, 0, 0, 0) );
 
     rect.x = rect.x + 1;
     rect.y = rect.y + 1;
     rect.w = rect.w - 2;
     rect.h = rect.h - 2;
-    DrawRect( screen, rect, SDL_MapRGB(screen->format, 192, 192, 192) );
+    Draw_Rect( screen, rect.x, rect.y, rect.w, rect.h, SDL_MapRGB(screen->format, 192, 192, 192) );
 }
