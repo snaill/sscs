1c86dde74d114de6a86bafbd98d61327b461476f
com.chinaos.snaill <com.chinaos.snaill@db7a99aa-9dca-11dd-b92e-bd4787f9249a>
2008-11-07 10:09:08 +0000

    添加测试项目，基于Google Test 添加SDL_Static类 添加SDL_LayoutObject 添加设置LayoutObject的接口

---
+++ SDL_Static.h
@@ -0,0 +1,36 @@
+/*
+ * SDL_SimpleControls
+ * Copyright (C) 2008 Snaill
+ *
+    SDL_SimpleControls is free software: you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SDL_SimpleControls is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Snaill  <snaill@jeebook.com>
+ */
+
+#ifndef SDL_STATIC_H_INCLUDED
+#define SDL_STATIC_H_INCLUDED
+
+#include "SDL_Widget.h"
+
+class SDL_Static : public SDL_Widget
+{
+// 实现父接口
+public:
+	SDL_Static( SDL_Glyph * pg );
+	virtual ~SDL_Static(void);
+
+	virtual const char * GetType()				{ return "label"; }
+};
+
+#endif //!SDL_STATIC_H_INCLUDED
