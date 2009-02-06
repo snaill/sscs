b41d7795d4bfdb0a3d62be7dfd40bafce9297686
com.chinaos.snaill <com.chinaos.snaill@db7a99aa-9dca-11dd-b92e-bd4787f9249a>
2009-02-06 11:02:20 +0000

    缓存字体以加快显示效率

--- trunk/SSC/source/SSC/SDL_Theme.h
+++ trunk/SSC/source/SSC/SDL_Theme.h
@@ -23,6 +23,7 @@
 
 #include "SDL_Object.h"
 #include "SDL_Font.h"
+#include <map>
 
 // @brief …Ë÷√¿‡
 class SDL_Theme : public SDL_Object
@@ -44,9 +45,14 @@ class SDL_DefaultTheme : public SDL_Theme
 {
 public:
     SDL_DefaultTheme()   {
+		m_pTextFont = 0;
+		m_pBigTextFont = 0;
     }
 
     ~SDL_DefaultTheme()  {
+		for ( std::map<int, SDL_Font *>::const_iterator pos = m_mapFonts.begin(); pos != m_mapFonts.end(); pos ++ )
+			(*pos).second->Release();
+		m_mapFonts.clear();
     }
 
 	virtual const char * GetType()	{ return "defaulttheme"; }
@@ -73,14 +79,28 @@ public:
 	}
 
 	SDL_Font * GetFont( int nParam )	{
-		switch ( nParam )
+		if ( m_mapFonts.find( nParam ) == m_mapFonts.end() )
 		{
-		case Text: return new SDL_Font( "C:\\Windows\\Fonts\\arial.ttf", 12 );
-		case BigText:return new SDL_Font( "C:\\Windows\\Fonts\\arial.ttf", 24 );
+			switch ( nParam )
+			{
+			case Text: 
+				m_mapFonts[ nParam ] = new SDL_Font( "C:\\Windows\\Fonts\\arial.ttf", 12 );
+				break;
+			case BigText:
+				m_mapFonts[ nParam ] = new SDL_Font( "C:\\Windows\\Fonts\\arial.ttf", 24 );
+				break;
+			default:
+				return 0;
+			}
 		}
 
-		return 0;
+		return ( SDL_Font * )m_mapFonts[ nParam ]->GetObj();
 	}
+
+protected:
+	std::map<int, SDL_Font *>	m_mapFonts;
+	SDL_Font *		m_pTextFont;
+	SDL_Font *		m_pBigTextFont;
 };
 
 #endif // SDL_THEME_H_INCLUDED
