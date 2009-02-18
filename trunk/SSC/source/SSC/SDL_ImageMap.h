531ac6e838e030df22a4f1a62dc4b00cec85d466
com.chinaos.snaill <com.chinaos.snaill@db7a99aa-9dca-11dd-b92e-bd4787f9249a>
2009-02-18 15:26:19 +0000

    增加图片接口,应该开始考虑是否删除ImageList 修改ImageMap的实现错误

--- trunk/SSC/source/SSC/SDL_ImageMap.h
+++ trunk/SSC/source/SSC/SDL_ImageMap.h
@@ -24,7 +24,7 @@
 #include "SDL_Object.h"
 #include "SDL_Image.h"
 #include <stdio.h>
-#include <map.h>
+#include <map>
 
 /// @brief ImageList
 class SDL_ImageMap : public SDL_Object
@@ -56,7 +56,7 @@ public:
 		if ( pos != m_images.end() )
 		{
 			(*pos).second->Release();
-			m_image.erase( pos );
+			m_images.erase( pos );
 		}
 	}
 
@@ -74,6 +74,35 @@ public:
 			return 0;
 
 		return ( SDL_Image * )(*pos).second->GetObj();
+	}
+	
+protected:
+	unsigned long GetHash32( const wchar_t * lpsz ) {
+		const unsigned int PRIME = 4294967291;
+
+		// h参数初始化为0，它用于调整hash值
+		unsigned int h = 0;
+		unsigned int g;
+
+		// 对p所指的字符串进行逐字符遍历
+		wchar_t *	p = ( wchar_t * )lpsz;
+		for( ; *p; ++p )
+		{
+			// h的值左移四位（相当于乘以16）再加上当前字符的ASCII码
+			h = ( h << 4 ) + *p;
+
+			// 将h值的高字节的前4位赋给g的相应位，g的其它位置0，并判断g（实际上是h的高字节的前4位）是否为0
+			if( g = h & 0xf0000000 )
+			{
+				// 如果为0，则将h的31~28位及7~4位依次与g作异或，结果传回h
+				h = h ^ ( g >> 24 );
+				h = h ^ g;
+			}
+			// 继续下一字符
+		}
+
+		// 返回h除PRIME的余数，PRIME实际上是哈希值的范围，通常认为取质数效果较好
+		return h % PRIME;
 	}
 protected:
 	ImageMap	m_images;
